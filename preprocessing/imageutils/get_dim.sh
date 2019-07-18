a5b0123685a9b9112301b09dd7943fcf230d629e
Matityahu Sarafzadeh <matthew2001@gmail.com>
2019-07-18 12:28:39 +0000

    mend

--- preprocessing/imageutils/get_dim.sh
+++
@@ -1,3 +0,0 @@
-# find . -name '*' -exec identify -format '%w %h\n' '{}' \; 
-#find . -name '*' -exec identify -format '%w %h\n' '{}' \; | awk -F' ' '$1 > 500 && $2 > 500' 
-find image-super-resolution/data/imagenet/orig/ -name '*' -exec identify -format '%i %w %h\n' '{}' \; | awk -F' ' '$2 > 300 && $3 > 300 {print;}' > ~/images_above_300 
--- preprocessing/imageutils/list_bad_images.sh
+++
@@ -1,5 +0,0 @@
-find image-super-resolution/data/imagenet/orig/ | while read FILE; do
-    if ! identify "$FILE" &> /dev/null; then
-        echo "$FILE"
-    fi  
-done
